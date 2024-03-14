acb72217c643d08b68088b1cd4d1ae50d802ab3d
KanchanCS <kanchan111yadav@gmail.com>
2024-03-14 23:07:02 +0530

    add

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,4 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib.auth.models import User
-
 from .models import Post, Subscription, MailMessage
 
 
@@ -10,31 +7,7 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ['title','text' ]
         
-class RegisterForm(UserCreationForm):
-    username = forms.CharField(required=True)
-    email = forms.EmailField(required=True)
-    required_css_class = "required"
 
-    class Meta:
-        model = User
-        fields = [
-            "username",
-            "first_name",
-            "last_name",
-            "email",
-            "password1",
-            "password2",
-        ]
-
-
-class LoginForm(AuthenticationForm):
-    
-    class Meta:
-        model = User
-        fields = (
-            "username",
-            "password",
-        )
 
 class SubscribeForm(forms.ModelForm):
     class Meta:
